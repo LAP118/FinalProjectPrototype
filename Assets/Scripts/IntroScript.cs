83ec633f60d09795d9ba59e4bf0ddaa9779534c4
LAP118 <97577291+LAP118@users.noreply.github.com>
2022-11-01 14:35:37 -0400

    temp

---
+++ Assets/Scripts/IntroScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class IntroScript : MonoBehaviour
+{
+
+    [SerializeField]
+    protected TextMeshProUGUI IntroMessageField;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+
+    protected void Ballmunch()
+    {
+       if (Time.time > 5f)
+       {
+            IntroMessageField.hide = true;
+       } 
+    }
+
+
+
+
+
+}
